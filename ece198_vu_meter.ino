a6c2ebc5fe63a4293ffc3f84ecf2b9c834e112d0
jbisarra3 <44039787+jbisarra3@users.noreply.github.com>
2020-03-25 03:22:03 -0700

    Add files via upload

---
+++ ece198_vu_meter.ino
@@ -0,0 +1,110 @@
+int mic_voltage = 0;
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 16 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+int bright_Pin = A2;
+int brightness;
+  void setup() {
+    pinMode(A0, INPUT);
+    pinMode(bright_Pin, INPUT);
+    Serial.begin(9600);
+    // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+    // Any other board, you can remove this part (but no harm leaving it):
+    #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+     clock_prescale_set(clock_div_1);
+    #endif
+    // END of Trinket-specific code.
+
+    pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  }
+  int previousCounter = 0;  
+  void loop() {
+    pixels.clear(); // Set all pixel colors to 'off'
+    int mic_voltage = analogRead(A0);
+    Serial.println(mic_voltage);
+    int value = mic_voltage - 504;
+    int counter = 0;
+    while (value > 0)
+    {
+      value = value - 1;
+      if (value < 0)
+      {
+        /*pixels.setPixelColor(counter, pixels.Color(3, 3, 3));
+          pixels.show();
+        */
+        break;
+      }
+      counter++;
+    }
+    fade(previousCounter,counter);
+    previousCounter = counter;
+  delay(100);
+  
+  
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  // to the count of pixels minus one.
+//for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+
+     //pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+//pixels.setPixelColor(i, pixels.Color(10, 10, 10));
+
+//pixels.show();   // Send the updated pixel colors to the hardware.
+
+//delay(DELAYVAL); // Pause before next pass through loop
+//}
+}
+
+void fade(int previousCounter, int counter)
+{
+  brightness = map(analogRead(bright_Pin), 0, 1023, 0, 255);
+  if(previousCounter > counter)
+  {
+     for(int i = 0; i < counter; i++)
+     {
+        pixels.setPixelColor(i, pixels.Color(3,3,3));
+        pixels.show();
+     }
+     while(previousCounter > counter)
+     {
+          pixels.setPixelColor(previousCounter, pixels.Color(0,0,0));
+        
+        pixels.show();
+        previousCounter--;
+        delay(10);
+        
+     }
+  }
+  else if(previousCounter < counter)
+  {
+     for(int i = 0; i < previousCounter; i++)
+     {
+        pixels.setPixelColor(i, pixels.Color(3,3,3));
+        pixels.show();
+     }
+    while(previousCounter < counter)
+    {
+      pixels.setPixelColor(previousCounter, pixels.Color(10,3,3));
+      pixels.show();
+      previousCounter++;
+      delay(10);
+      
+    }
+  }
+}
---
+++ spectrometer.ino
@@ -0,0 +1,93 @@
+
+#include <LedControl.h>
+int strobePin = 6;
+int resetPin = 9;
+int outPin = A3;
+int level[7];
+
+
+const int DATA = 2;
+const int CLOCK = 3;
+const int CHIP_SET = 4;
+const int DEVICE_NUMBER = 1;
+LedControl lc=LedControl(DATA, CLOCK, CHIP_SET, DEVICE_NUMBER);
+
+void setup() {
+
+   lc.shutdown(0,false); //Turn on Matrix
+  lc.setIntensity(0,8); //Set Brightness
+  lc.clearDisplay(0); //Clear Display
+  
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  pinMode (strobePin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+  pinMode(outPin, INPUT);
+
+  digitalWrite(resetPin, LOW);
+  digitalWrite(strobePin,LOW);
+  delay(1);
+
+  digitalWrite(resetPin, HIGH);
+  delay(1);
+  digitalWrite(resetPin, LOW);
+  digitalWrite(strobePin, HIGH);
+  delay(1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  int mic_voltage = analogRead(A5);
+//  Serial.println(mic_voltage);
+//  delay(10);
+
+  digitalWrite(resetPin, HIGH);
+  delay(1);
+  digitalWrite(resetPin, LOW);
+
+  for (int i = 0; i < 7; i++){
+    digitalWrite(strobePin, LOW);
+    delayMicroseconds(1000);
+    level[i] = analogRead(outPin);
+    digitalWrite(strobePin, HIGH);
+    delayMicroseconds(1000);
+  }
+
+/*  for(int i = 0; i < 8; i++)
+  {
+    int lights[] = {0,1,3,7,15,31,63,127,255};
+    if (level[i] > 500){
+      lc.setColumn(0,i,lights[7]);
+    } 
+    else {
+      lc.setRow(0, i, lights[0]);
+    }
+  }
+*/
+    for(int i = 7; i >= 0; i--)
+    {
+      int lights[] = {0,1,3,7,15,31,63,127,255};
+      int currentLevel = level[i];
+      int threshold = 128;
+      int ledLevel = 0;
+      while(currentLevel > 128)
+      {
+        currentLevel = currentLevel - 90;
+        lc.setLed(0,i,ledLevel,1);
+        ledLevel++; 
+      }
+      
+    }
+    Serial.println();
+    delay(100);
+    lc.clearDisplay(0);
+
+  for (int i = 0; i < 7; i++){
+   Serial.print(level[i]);
+   Serial.print("  ");
+  }
+
+  Serial.println();
+
+}
